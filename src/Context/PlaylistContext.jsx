3fb8a715c03ac452dbe26f2d4761b9d95fa6fd65
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2025-05-08 15:29:29 -0600

    Create playlist context logic

---
+++ src/Context/PlaylistContext.jsx
@@ -0,0 +1,41 @@
+import { createContext, useContext, useReducer } from "react";
+
+const PlaylistContext = createContext(null);
+
+const PlaylistDispatchContext = createContext(null);
+
+export function PlaylistProvider({ children }) {
+  const [playlist, dispatch] = useReducer(playlistReducer, initialplaylist);
+
+  return (
+    <PlaylistContext.Provider value={playlist}>
+      <PlaylistDispatchContext.Provider value={dispatch}>
+        {children}
+      </PlaylistDispatchContext.Provider>
+    </PlaylistContext.Provider>
+  );
+}
+
+export function usePlaylist() {
+  return useContext(PlaylistContext);
+}
+
+export function usePlaylistDispatch() {
+  return useContext(PlaylistDispatchContext);
+}
+
+function playlistReducer(playlist, action) {
+  switch (action.type) {
+    case "add": {
+      if (playlist.includes(action.track)) return playlist;
+      return [...playlist, action.track];
+    }
+    case "remove": {
+      return playlist.filter((t) => t.id !== action.track.id);
+    }
+  }
+}
+
+const initialplaylist = [
+  { id: 4, name: "Song D", artist: "Artist D", album: "Album D" },
+];
