e5e915cf63d30b4e1ba333bf14790330b21616c0
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2025-05-16 13:05:04 -0600

    Add code for requesting user authorization from spotify

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./App.module.css";
 import SearchResults from "./components/SearchResults/SearchResults";
 import Playlist from "./components/Playlist/Playlist";
 import SearchBar from "./components/SearchBar/Searchbar";
 import { PlaylistProvider } from "./Context/PlaylistContext";
 import { StyledEngineProvider } from "@mui/material";
+import { base64encode, generateRandomString, sha256 } from "./Helpers/helpers";
 
 // Mock data
 const mockSearchResults = [
@@ -15,6 +16,39 @@ const mockSearchResults = [
 
 function App() {
   const [searchResults, setSearchResults] = useState(mockSearchResults);
+  const [codeChallenge, setCodeChallenge] = useState("");
+
+  useEffect(() => {
+    const generateCodeChallenge = async () => {
+      const verifier = generateRandomString(64);
+      const hashed = await sha256(verifier);
+      const challenge = base64encode(hashed);
+      setCodeChallenge(challenge);
+      window.localStorage.setItem("code_verifier", verifier);
+    };
+    generateCodeChallenge();
+
+    const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
+    const redirectUri = "http://127.0.0.1:5173/callback";
+
+    const scope = "user-read-private user-read-email";
+    const authUrl = new URL("https://accounts.spotify.com/authorize");
+
+    const params = {
+      response_type: "code",
+      client_id: clientId,
+      scope,
+      code_challenge_method: "S256",
+      code_challenge: codeChallenge,
+      redirect_uri: redirectUri,
+    };
+    const urlParams = new URLSearchParams(window.location.search);
+    let code = urlParams.get("code");
+    if (!code) {
+      authUrl.search = new URLSearchParams(params).toString();
+      window.location.href = authUrl.toString();
+    }
+  });
 
   return (
     <StyledEngineProvider injectFirst>
