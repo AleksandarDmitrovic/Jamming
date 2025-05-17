8c1e7880587b0ba14b1c790934cf817686c66cd9
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2025-05-17 12:17:21 -0600

    Add searchSpotify api call

--- src/Helpers/Helpers.js
+++ src/Helpers/Helpers.js
@@ -17,3 +17,46 @@ export const base64encode = (input) => {
     .replace(/\+/g, "-")
     .replace(/\//g, "_");
 };
+
+export const getSpotifyAccessToken = async (code, codeVerifier) => {
+  const url = "https://accounts.spotify.com/api/token";
+  const payload = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      client_id: import.meta.env.VITE_SPOTIFY_CLIENT_ID,
+      grant_type: "authorization_code",
+      code,
+      redirect_uri: import.meta.env.VITE_SPOTIFY_REDIRECT_URI,
+      code_verifier: codeVerifier,
+    }),
+  };
+
+  const body = await fetch(url, payload);
+  const response = await body.json();
+
+  return response;
+};
+
+export const searchSpotify = async (token, query) => {
+  const params = new URLSearchParams({
+    q: query,
+    type: "track",
+    limit: 10,
+  });
+  const url = `https://api.spotify.com/v1/search?${params}`;
+  const payload = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  };
+
+  const body = await fetch(url, payload);
+  const response = await body.json();
+
+  return response;
+};
