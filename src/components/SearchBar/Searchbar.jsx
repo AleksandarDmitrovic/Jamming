6ca930e5d19e6e6d1b8c470c34ef3401b1e58a28
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2025-05-17 12:18:01 -0600

    Call search Spotify api call on search btn click

--- src/components/SearchBar/Searchbar.jsx
+++ src/components/SearchBar/Searchbar.jsx
@@ -1,11 +1,37 @@
 import React from "react";
 import styles from "./SearchBar.module.css";
+import { searchSpotify } from "../../Helpers/helpers";
 
-function SearchBar() {
+function SearchBar({
+  onChange,
+  searchQuery,
+  accessTokenData,
+  setSearchResults,
+}) {
+  const handleChange = (event) => {
+    onChange(event.target.value);
+  };
+
+  const handleSearch = async () => {
+    if (!accessTokenData) return;
+    const response = await searchSpotify(
+      accessTokenData.access_token,
+      searchQuery
+    );
+    console.log("response :", response);
+    setSearchResults(response.tracks.items);
+  };
+
   return (
     <div className={styles.searchBar}>
-      <input placeholder="Enter A Song Title" />
-      <button className={styles.searchButton}>SEARCH</button>
+      <input
+        value={searchQuery}
+        placeholder="Enter A Song Title"
+        onChange={handleChange}
+      />
+      <button className={styles.searchButton} onClick={handleSearch}>
+        SEARCH
+      </button>
     </div>
   );
 }
